8b3447ba0328fa7c60eee7893654889a49f7f6e8
sona <simset1212@gmail.com>
2023-01-03 23:58:53 +0900

    Hashing?

---
+++ day17/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <bitset>
+using namespace std;
+
+using pos_type = pair<long long, long long>;
+const int N_cols = 7;
+
+struct Map {
+    vector<bitset<N_cols>> blocks; 
+};
+
+struct Shape {
+    int rows = 0;
+    int cols = 0;
+    vector<bool> blocks;
+};
+
+bool detect_collision(const Map& map, const Shape& shape, const pos_type& pos) {
+    if(pos.second < 0) {
+        return true;
+    }
+
+    if(pos.first < 0) {
+        return true;
+    }
+}
